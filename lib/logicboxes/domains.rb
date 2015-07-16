baf7adb7db5556c5ca63c08e32f534dd6668a125
Sarun Intaralawan <sarun.intaralawan@gmail.com>
2015-07-16 15:29:55 +0700

    Bug fix

--- lib/logicboxes/domains.rb
+++ lib/logicboxes/domains.rb
@@ -2,7 +2,7 @@ require 'logicboxes'
 require 'json'
 module Logicboxes
   class Domains
-    def search(args)
+    def self.search(args)
       args['no-of-records'] = 500
       args['page-no'] = 1
       a = Logicboxes::LogicboxesConnector.send('/api/domains/search.json', args, :get)
